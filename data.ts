c8ffd1499d9853248c77e33d2771dfa85cc4a1e9
Awoms Kelechi <awomskelechi@gmail.com>
2024-01-14 19:24:13 +0100

    changed services card background color for dark mode

--- data.ts
+++ data.ts
@@ -13,7 +13,7 @@ export const projects: ProjectType[] = [
     id: 1,
     icon: BsFileEarmarkPerson,
     text1: "Web Design",
-    text2: "My goal is to help businesses and individuals establish a strong online presence and effectively communicate their brand to their target audience."
+    text2: "I help businesses and companies establish a strong online presence and effectively communicate their brand to their target audience."
   },
 
   {
@@ -26,7 +26,7 @@ export const projects: ProjectType[] = [
     id: 3,
     icon: BsFileEarmarkPerson,
     text1: "Api Integration",
-    text2: "I can create and integrate 3rd party APIs into web apps that help keep data in sync, automate tasks, enhance productivity & drive more revenue.",
+    text2: "I can create and integrate APIs into web apps that help keep data in sync, automate tasks, enhance productivity & drive more revenue.",
   },
   {
     id: 4,
