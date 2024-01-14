8b4eb47866035a5fd6e02b071a7f6f344105600d
Awoms Kelechi <awomskelechi@gmail.com>
2024-01-14 18:44:28 +0100

    modified styling

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -13,13 +13,13 @@ export default function Home() {
         <h1 className="hero-main-text">
           I'm Kelechi. I'm a fullstack web developer
         </h1>
-        {/* <h3 className="hero-sub-text">
-          I am smart, hardworking and always willing to do what is required to
+        <h3 className="hero-sub-text">
+          I am always willing to work hard to
           achieve the goals of my employer.
-        </h3> */}
+        </h3>
 
         <div className="flex justify-center">
-          <h2 className="services-header">Services I render</h2>
+          <h2 className="services-header">My Services</h2>
         </div>
       </section>
 
@@ -32,7 +32,7 @@ export default function Home() {
               <Icon size={40} className="text-emerald-600 " />
 
               <h2 className="text-xl font-semibold mt-6 text-emerald-600">{project.text1}</h2>
-              <p className="text-base mt-2 font-normal text-emerald-950 dark:text-white">{project.text2}</p>
+              <p className="text-base mt-2 text-emerald-950 dark:text-white">{project.text2}</p>
             </div>
           );
         })}
