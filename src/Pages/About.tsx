30cac1241322606cabf1aaffc8d64722eb23735c
Awoms Kelechi <awomskelechi@gmail.com>
2024-01-16 10:28:48 +0100

    modified styling

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -10,49 +10,36 @@ const About = () => {
       <Title label="ABOUT MYSELF" />
       <Subtitle label="Basic Info:"/>
       <p className="dark:text-white font-normal text-emerald-900 mb-6">
-        I am a fullstack web developer fron Nigeria.  I am goal-driven, ambitious, and seasoned developer with experience in
-        shipping features for large-scale and real-time applications, as well as
-        building consumer-facing products. I am actively seeking a frontend role
+        I am a fullstack web developer fron Nigeria.  I am goal-driven, ambitious, and seasoned developer with experience in shipping features for large-scale and real-time applications, as well as
+        building consumer-facing products. I am actively seeking a frontend or backend role
         in a growing company where I can apply my creative mindset and keen
         ability to work on diverse projects.
       </p>
       
       <Subtitle label="Work Experience:"/>
       <p className=" dark:text-white font-normal text-emerald-900 mb-6">
-        With my experience in the industry, I have developed strong
+        I have worked for two years as a freelance frontend developer on upwork where I was responsible for architecting and building the frontend of new features, integrating data from various
+        backend services and databases into responsive web apps, translating
+        design solutions into high-quality, scalable code, optimizing
+        applications for maximum performance across different web-capable
+        devices and browsers. During this period, I have developed strong
         problem-solving skills, attention to detail, and the ability to work
         independently. I am confident in my ability to tackle complex challenges
         and complete projects to a high standard and within deadlines. My
-        goal-driven attitude and passion for delivering high-quality work make
+        goal-driven attitude and passion for delivering high-quality work makes
         me an ideal candidate for any team in need of a dynamic and
         result-oriented individual.
       </p>
       <p className=" dark:text-white font-normal text-emerald-900 mb-6">
-        I am currently working as a freelancer. In this role, I have been responsible for architecting and
-        building the frontend of new features, integrating data from various
-        backend services and databases into responsive web apps, translating
-        design solutions into high-quality, scalable code, optimizing
-        applications for maximum performance across different web-capable
-        devices and browsers, collaborating with team members to implement user
-        stories and write tests, and participating in code reviews to ensure
+        I have also collaborated with other web developers on open source projects, where I added functionality to web application, wrote tests, and participated in code reviews to ensure
         adherence to best practices and coding standards.
       </p>
-      <p className=" dark:text-white font-normal text-emerald-900 mb-6">
-        My technical skills include HTML, CSS, JavaScript, TypeScript, React.js,
-        Next.js, MongoDB, Tailwind CSS, Git, Firebase, Redux/Redux-Toolkit,
-        React-Query, Zod, and Next-Auth/Auth.js. These skills cover a broad
-        range of frontend development technologies and frameworks, showcasing my
-        versatility and ability to adapt to different projects and environments.
-      </p>
+      
       
       <Subtitle label="Hobbies:"/>
       <p className=" dark:text-white font-normal text-emerald-900 mb-6">
-        Beyond my professional life, I have a range of interests which includes
-        reading, traveling, and listening to music. I have obtained
-        certifications in Responsive Web Design and Frontend Development
-        Libraries. Additionally, I have completed Jobberman Soft Skills Training
-        Certification, I believe these certifications will further enhance my
-        professional skill set.
+        Beyond my professional life, I have a range of interests which include
+        reading, writing stories, playing the piano, listening to music and playing football. I am also an ardent follower of sports, particularly tennis, football and track and field athletics. I also have a keen interest  in ancient history.
       </p>
     </Wrapper>
   );
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -15,14 +15,14 @@ export default function Home() {
         <h1 className="text-4xl md:text-6xl lg:text-7xl text-cyan-600 font-serif text-center">
           I am Kelechi
         </h1>
-        <h1 className="text-2xl mt-2 md:text-2xl lg:text-5xl text-cyan-800 tracking-wide dark:text-white text-center">
+        <h1 className="text-2xl mt-2 md:text-2xl lg:text-5xl text-cyan-600 tracking-wide dark:text-white text-center">
           Fullstack Web Developer
         </h1>
         
 
         <div className="flex justify-center">
           <h2 className="lg:mt-20 mt-10 mx-2 mb-1 py-2 px-4 font-medium md:font-semibold  bg-cyan-600 dark:bg-cyan-600  text-white rounded-full text-sm flex items-center w-fit">
-            <span>Services I Render</span>
+            <span>My Services</span>
             <BiChevronsDown size={22} />
           </h2>
         </div>
