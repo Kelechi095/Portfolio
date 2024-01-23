80e1a5be8fb4c94a8d9281a292387f4555206ea0
Awoms Kelechi <awomskelechi@gmail.com>
2024-01-23 11:03:35 +0100

    modified skils page

--- src/Pages/Skills.tsx
+++ src/Pages/Skills.tsx
@@ -1,87 +1,68 @@
-
 import ListItem from "../components/ListItem";
 import Subtitle from "../components/Subtitle";
-import Wrapper from "../components/Wrapper";
-import { GiSkills } from "react-icons/gi";
+import Wrapper from "../components/Wrapper"
 import { TbBrandNextjs } from "react-icons/tb";
 import { SiPrisma } from "react-icons/si";
 import { IoLogoJavascript } from "react-icons/io";
 import { BiLogoPostgresql } from "react-icons/bi";
 import { DiMongodb } from "react-icons/di";
 
-import Javascript from '../public/images/javascript.png'
-import Html from '../public/images/html.png'
-import Css from '../public/images/css.png'
-import Typescript from '../public/images/typescript.png'
-import Node from '../public/images/node.png'
-import React from '../public/images/react.png'
-import Redux from '../public/images/redux.png'
-import Tailwind from '../public/images/tailwind.png'
-import Git from '../public/images/git.png'
-import Npm from '../public/images/npm.png'
-import Jest from '../public/images/jest.png'
-import Axios from '../public/images/axios.png'
-import ReactQuery from '../public/images/reactquery.png'
+import Javascript from "../public/images/javascript.png";
+import Html from "../public/images/html.png";
+import Css from "../public/images/css.png";
+import Typescript from "../public/images/typescript.png";
+import Node from "../public/images/node.png";
+import React from "../public/images/react.png";
+import Redux from "../public/images/redux.png";
+import Tailwind from "../public/images/tailwind.png";
+import Git from "../public/images/git.png";
+import Npm from "../public/images/npm.png";
+import Jest from "../public/images/jest.png";
+import Axios from "../public/images/axios.png";
+import ReactQuery from "../public/images/reactquery.png";
 import Title from "../components/Title";
-
-
 
 const Skills = () => {
   return (
     <Wrapper>
       <Title label="Skills" />
-      
 
-      <section className="grid lg:grid-cols-2 grid-cols-1 gap-4 mb-8">
-        <div className="py-2 px-6 bg-slate-800 shadow-sm rounded-md">
+      <section className="grid lg:grid-cols-2 grid-cols-1 gap-4 mb-8 w-full px-2">
+        <div className="py-2">
           <Subtitle label="Frontend" />
-          <ul>
+          <ul className="grid grid-cols-3 gap-4">
             <ListItem text="HTML5" src={Html} />
             <ListItem text="CSS3" src={Css} />
-            <ListItem text="JavaScript(ES6+)" src={Javascript} />
+            <ListItem text="JavaScript" src={Javascript} />
             <ListItem text="TypeScript" src={Typescript} />
             <ListItem text="ReactJs" src={React} />
             <ListItem text="NextJs" icon={TbBrandNextjs} black />
           </ul>
         </div>
 
-        <div className="py-2 px-6 bg-slate-800 shadow-sm rounded-md">
+        <div className="py-2">
           <Subtitle label="Backend" />
-          <ul>
+          <ul className="grid grid-cols-3 gap-4">
             <ListItem text="NodeJs" src={Node} />
-            <ListItem text="Express" icon={IoLogoJavascript} blue/>
-            <ListItem text="PostgreSQL" icon={BiLogoPostgresql} blue/>
-            <ListItem text="MongoDB" icon={DiMongodb} mongo/>
-            <ListItem text="Prisma" icon={SiPrisma} black/>
+            <ListItem text="Express" icon={IoLogoJavascript} blue />
+            <ListItem text="PostgreSQL" icon={BiLogoPostgresql} blue />
+            <ListItem text="MongoDB" icon={DiMongodb} mongo />
+            <ListItem text="Prisma" icon={SiPrisma} black />
           </ul>
         </div>
 
-        <div className="bg-slate-800 py-2 px-6 shadow-sm rounded-md hover:scale-y-100">
+        <div className="py-2">
           <Subtitle label="Tools & Libraries" />
-          <ul>
+          <ul className="grid grid-cols-3 gap-4">
             <ListItem text="React Query" src={ReactQuery} />
-            <ListItem text="Redux tool kit" src={Redux} />
-            <ListItem text="Tailwind CSS" src={Tailwind} />
+            <ListItem text="Redux" src={Redux} />
+            <ListItem text="Tailwind" src={Tailwind} />
             <ListItem text="Axios" src={Axios} />
             <ListItem text="Git" src={Git} />
             <ListItem text="NPM" src={Npm} />
             <ListItem text="Jest" src={Jest} />
           </ul>
         </div>
-
-        <div className=" py-2 px-6 bg-slate-800 shadow-sm rounded-md">
-          <Subtitle label="Soft Skills" />
-          <ul>
-            <ListItem text="Good communication skills" icon={GiSkills} blue/>
-            <ListItem text="Critical thinking and problem solving" icon={GiSkills} blue/>
-            <ListItem text="Team work and collaboration" icon={GiSkills} blue/>
-            <ListItem text="Strong work ethic" icon={GiSkills} blue/>
-            <ListItem text="Flexibility and adaptability" icon={GiSkills} blue/>
-            <ListItem text="Time management skills" icon={GiSkills} blue/>
-          </ul>
-        </div>
-
-        
       </section>
     </Wrapper>
   );
--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -14,14 +14,14 @@ interface ListItemProps {
 const ListItem = ({text, src, icon: Icon, black, mongo, blue }: ListItemProps) => {
     
   return (
-    <li className="flex items-center text-base gap-1">
+    <li className="flex  flex-col justify-around items-center h-20 rounded px-8 py-2 bg-slate-800">
       {Icon && <Icon size={20} className={`
         ${black && " text-black dark:text-white"}
         ${mongo && "text-lime-500"}
         ${blue && "text-cyan-700"}
         `}/>}
-        {src && <img src={src ? src : '/images/javascript.png'} alt="logo"  className="w-[16px]" />}
-        <span className="text-white font-normal text-sm">{text}</span>
+        {src && <img src={src ? src : '/images/javascript.png'} alt="logo"  className="w-[20px]" />}
+        <span className="text-white font-normal text-xs md:text-base">{text}</span>
     </li>
   )
 }
